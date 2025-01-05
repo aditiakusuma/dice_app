0553e5d7689616074bd8e344f84cdb57a4ee1632
aditiakusuma <aditiakusuma800@gmail.com>
2025-01-05 14:08:34 +0700

    separate

---
+++ lib/gradient_container.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 255, 0, 0),
+          Color.fromARGB(255, 119, 0, 0)
+        ], begin: Alignment.topLeft)),
+        child: Center(
+            child: Text(
+          'Test',
+          style: TextStyle(fontSize: 28, color: Colors.white),
+        )));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,10 @@
+import 'package:dice_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: MainPage(),
+      body: GradientContainer(),
     ),
   ));
-}
-
-class MainPage extends StatelessWidget {
-  const MainPage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 255, 0, 0),
-          Color.fromARGB(255, 119, 0, 0)
-        ], begin: Alignment.topLeft)),
-        child: Center(
-            child: Text(
-          'Test',
-          style: TextStyle(fontSize: 28, color: Colors.white),
-        )));
-  }
 }
